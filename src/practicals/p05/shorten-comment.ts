c8776a57c109574d4a2ed90927dd739c21876065
ossup <os.supawit@gmail.com>
2026-01-21 17:00:55 +0700

    d

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,10 @@
+export{};
 const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
-  // Write your code below
+  const ftword = comment.split(' ');
+  const ft2word = ftword.filter(word => word.length >= 5 && word.length <= 10)
+  const newstring = ft2word.join(' ');
+  return newstring
 }
+console.log(shortenComment(comment))
--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,21 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
-}
-
+export{};
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+
+
+function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+  let arr3=[];
+  for (let i=0;i<arr1.length;i++) {
+    for(let y=0;y<arr2.length;y++){
+      if (arr1[i] !== arr2[y]){
+        arr3.push(arr2[y]);
+      }
+    }
+    
+
+    
+  }
+  return arr3;
+}
+console.log(getUniqueNumbers(arr1,arr2))
+
