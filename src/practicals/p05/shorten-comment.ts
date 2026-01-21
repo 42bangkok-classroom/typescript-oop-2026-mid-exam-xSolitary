3fa03c62245c287884d18aa0f7b7aae2cdf2ef95
ossup <os.supawit@gmail.com>
2026-01-21 17:01:55 +0700

    d

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -2,9 +2,13 @@ export{};
 const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
+  if(comment === ""){
+    return"";
+  }
   const ftword = comment.split(' ');
   const ft2word = ftword.filter(word => word.length >= 5 && word.length <= 10)
   const newstring = ft2word.join(' ');
   return newstring
+  
 }
 console.log(shortenComment(comment))
