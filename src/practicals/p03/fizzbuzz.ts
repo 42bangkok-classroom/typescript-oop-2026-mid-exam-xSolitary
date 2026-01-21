cc887ec3162216f2d0d771468a20e8727063eca0
ossup <os.supawit@gmail.com>
2026-01-21 14:36:45 +0700

    os

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,12 @@
-// Write your code below
+export {};
+const arg = process.argv[2];
+const n = Number(arg);
+
+if (arg === undefined || Number.isNaN(n) || n <= 0) process.exit(0);
+
+for (let i = 1; i <= n; i++) {
+  if (i % 15 === 0) console.log("FizzBuzz");
+  else if (i % 3 === 0) console.log("Fizz");
+  else if (i % 5 === 0) console.log("Buzz");
+  else console.log(i);
+}
