b14881514adc58fbf0b36d323b2f13b6f1f58f0c
ossup <os.supawit@gmail.com>
2026-01-21 14:59:31 +0700

    os

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,18 @@
-// Write your code below
+export {};
+const arg = process.argv[2];
+const n = Number(arg);
+
+if (arg === undefined || Number.isNaN(n) || n < 0) process.exit(0);
+
+
+
+for(let i=0;i<n;i++){
+  let row = ""
+  for(let y=i+1;y<n;y++){
+    row+=" ";
+  }
+  for(let j=i+1;j>0;j--){
+    row+=j;
+  }
+  console.log(row)
+}
